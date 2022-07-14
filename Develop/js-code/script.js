9e4cb932e4260daa61ee6950f553d7a819be6bc9
nashalysf <nashalysf@gmail.com>
2022-07-14 19:22:55 -0400

    save btn function added

--- Develop/js-code/script.js
+++ Develop/js-code/script.js
@@ -9,5 +9,12 @@ $(document).ready(function() {
     var milHour = moment().format("HH")
     var businessHoursMil = ["09", "10", "11", "12", "13", "14", "15", "16", "17"]
 
-  
+  //save function 
+  $(".saveBtn").click(function() {
+    var textSave = $.trim($(this).siblings("textarea").val());
+    var newSave = {text: textSave, hour: this.id}
+    storedAgenda.push(newSave)
+    window.localStorage.setItem("agendaSave", JSON.stringify(storedAgenda))
+})
+
 })
